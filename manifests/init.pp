754d5d542f9a56389d447e059f4696617934c20c
Jordi Prats <jprats@systemadmin.es>
2017-02-28 14:11:00 +0100

    dependencia paquet

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,13 @@
 class apt() inherits apt::params {
 
+  package { 'software-properties-common':
+    ensure => 'installed',
+  }
+
   exec { 'eyp-apt apt-get update':
     command     => 'apt-get update',
     path        => '/usr/sbin:/usr/bin:/sbin:/bin',
     refreshonly => true,
+    require     => Package['software-properties-common'],
   }
 }
