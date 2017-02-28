92598e7eed5cb3bd8ada8eb22657a2cf09bea984
Jordi Prats <jprats@systemadmin.es>
2017-02-28 12:28:52 +0100

    ppa

---
+++ manifests/ppa.pp
@@ -0,0 +1,30 @@
+define apt::ppa (
+                  $ensure       = 'present',
+                  $package_name = $name,
+                ) {
+
+  # ppa:dontblamenrpe/ppa
+  if $package_name =~ /^ppa:(.*)$/
+  {
+    $package_url = $1
+  }
+  else
+  {
+    fail('incorrect PPA name format')
+  }
+
+  case $ensure
+  {
+    'present':
+    {
+      exec { "ppa install ${package_name}":
+        command => "add-apt-repository ${package_name}",
+        unless  => "apt-cache policy | grep ${package_url}",
+      }
+    }
+    default:
+    {
+      fail("unsupported state: ${ensure}")
+    }
+  }
+}
