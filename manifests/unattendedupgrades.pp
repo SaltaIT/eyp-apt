7ed0c05415ce8dce87968b33d98f5e8e87fecefb
Jordi Prats <jordi.prats@gmail.com>
2019-04-26 13:40:19 +0200

    neteja

--- manifests/unattendedupgrades.pp
+++ manifests/unattendedupgrades.pp
@@ -1,10 +1,6 @@
 class apt::unattendedupgrades(
                               $manage_package        = true,
                               $package_ensure        = 'installed',
-                              $manage_service        = true,
-                              $manage_docker_service = true,
-                              $service_ensure        = 'running',
-                              $service_enable        = true,
                             ) inherits apt::params{
   include ::apt
 
